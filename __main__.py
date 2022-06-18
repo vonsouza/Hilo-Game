933a0820672056bab060be7d1e56fd845e9e69f3
Von Souza <sou21002@byui.edu>
2022-06-17 21:24:32 -0300

    Update __main__.py
    correcting comments

--- __main__.py
+++ __main__.py
@@ -1,12 +1,15 @@
-from game.director import Director
+from director import Director
 
 # Hilo is a game in which the player guesses if the next card drawn by the dealer will be higher or lower 
 # than the previous one. Points are won or lost based on whether or not the player guessed correctly.
 
-# Upgrad necessary:
-# Don't allow the player to choose a number greater than 9 or less than 1
-# Don't allow the player to choose letters
-# Don't allow the player to choose numbers already chosen
+# CSE 210 | Programming with Classes
+# Week 02
+#
+# @vonsouza
+# 06/17/2022
+#
+# Hilo Game
 
 director = Director()
 director.start_game()
